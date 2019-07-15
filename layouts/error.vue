a977ec1a9cd5f8b3aa78c933fd16432b24d831bd
Renatas <47356419+Duce201@users.noreply.github.com>
2019-07-15 20:40:43 +0200

    Add files via upload

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app app>
+
+  <v-content>
+    <v-container fluid>
+     <h1>Страница не найдена</h1>
+    </v-container>
+  </v-content>
+  <v-footer app></v-footer>
+</v-app>
+</template>
+
+
